2a67f00bb3f42565fd82fca32c5c1e576452ac99
agashevishwas189-creator <agashevishwas189@gmail.com>
2026-03-27 17:34:55 +0530

    new-tf-adding

---
+++ teraform/resource.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "dummy" {
+    ami = "ami-0f559c3642608c138"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = ["sg-0c78535db3865697a",aws_security_group.my_sg.id]
+
+    tags = {
+        Name = "dummy"
+    }
+
+}
+resource "aws_security_group" "my_sg" {
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress { 
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    tags = {
+      name = my-sg
+    }   
+}
